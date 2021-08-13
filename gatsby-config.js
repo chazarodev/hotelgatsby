e8cbe865054d3798ea9d7fb07be46c455c3a2b6f
Alberto Cházaro <shazarerom@gmail.com>
2021-08-12 23:57:26 -0500

    Agregando información desde CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,8 +31,11 @@ module.exports = {
       },
     },
     `gatsby-plugin-gatsby-cloud`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-source-datocms',
+      options: {
+        apiToken: 'c13865de807d95c3d9a4734574c35a',
+      }
+    }
   ],
 }
